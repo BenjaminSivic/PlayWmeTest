af319e48146c87c8b091a4f594965bed537f5864
Benjamin Sivic <74781281+BenjaminSivic@users.noreply.github.com>
2023-08-21 19:20:18 +0000

    Made routes go through mongoDB, changed controllers accordingly.

--- BackEndServer/controllers/employeesController.js
+++ BackEndServer/controllers/employeesController.js
@@ -1,64 +1,75 @@
-const data = {
-    employees : require('../model/employees.json'),
-    setEmployees:function (data) {this.employees = data}
-}
+const Player = require("../model/Player");
 
+const getAllEmployees = async (req, res) => {
+  const players = await Player.find();
+  if (!players) return res.status(204).json({ message: "No players found" });
+  res.json(players);
+};
 
-const getAllEmployees = (req, res)=>{
-    res.json(data.employees);
-}
+const createNewEmployee = async (req, res) => {
+  if (!req?.body?.firstname || !req?.body?.lastname) {
+    return res.status(400).json({ message: "First and last name required" });
+  }
 
+  try {
+    const result = await Player.create({
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+    });
 
-const createNewEmployee = (req, res) => {
-    const newEmployee = {
-        id: data.employees?.length ? data.employees[data.employees.length - 1].id + 1 : 1,
-        firstname: req.body.firstname,
-        lastname: req.body.lastname
-    }
+    res.status(201).json(result);
+  } catch (err) {
+    console.error(err);
+  }
+};
 
-    if (!newEmployee.firstname || !newEmployee.lastname) {
-        return res.status(400).json({ 'message': 'First and last names are required.' });
-    }
+const updateEmployee = async (req, res) => {
+  if (!req?.body?.id) {
+    return res.status(400).json({ message: "ID is required" });
+  }
 
-    data.setEmployees([...data.employees, newEmployee]);
-    res.status(201).json(data.employees);
-}
+  const player = await Player.findOne({ _id: req.body.id }).exec();
 
-const updateEmployee = (req, res)=>{
-    const employee = data.employees.find(emp => emp.id === parseInt(req.body.id));
-    if (!employee) {
-        return res.status(400).json({ "message": `Employee ID ${req.body.id} not found` });
-    }
-    if (req.body.firstname) employee.firstname = req.body.firstname;
-    if (req.body.lastname) employee.lastname = req.body.lastname;
-    const filteredArray = data.employees.filter(emp => emp.id !== parseInt(req.body.id));
-    const unsortedArray = [...filteredArray, employee];
-    data.setEmployees(unsortedArray.sort((a, b) => a.id > b.id ? 1 : a.id < b.id ? -1 : 0));
-    res.json(data.employees);
-}
+  if (!player) {
+    return res
+      .status(204)
+      .json({ message: `Player ID ${req.body.id} not found` });
+  }
+  if (req.body?.firstname) player.firstname = req.body.firstname;
+  if (req.body?.lastname) player.lastname = req.body.lastname;
+  const result = await player.save();
+  res.json(result);
+};
 
-const delteEmployee = (req,res)=>{
-    const employee = data.employees.find(emp => emp.id === parseInt(req.body.id));
-    if (!employee) {
-        return res.status(400).json({ "message": `Employee ID ${req.body.id} not found` });
-    }
-    const filteredArray = data.employees.filter(emp => emp.id !== parseInt(req.body.id));
-    data.setEmployees([...filteredArray]);
-    res.json(data.employees);
-}
+const delteEmployee = async (req, res) => {
+  if (!req?.body?.id)
+    return res.status(400).json({ message: "Player ID required" });
+  const player = await Player.findOne({ _id: req.body.id }).exec();
+  if (!player) {
+    return res
+      .status(204)
+      .json({ message: `Player ID ${req.body.id} not found` });
+  }
+  const result = await player.deleteOne({ _id: req.body.id });
+  res.json(result);
+};
 
-const getEmployee = (req,res)=>{
-    const employee = data.employees.find(emp => emp.id === parseInt(req.params.id));
-    if (!employee) {
-        return res.status(400).json({ "message": `Employee ID ${req.params.id} not found` });
-    }
-    res.json(employee);
-}
+const getEmployee = async (req, res) => {
+  if (!req?.params?.id)
+    return res.status(400).json({ message: "Player ID required" });
+  const player = await Player.findOne({ _id: req.params.id }).exec();
+  if (!player) {
+    return res
+      .status(204)
+      .json({ message: `Player ID ${req.params.id} not found` });
+  }
+  res.json(player);
+};
 
 module.exports = {
-    getAllEmployees,
-    createNewEmployee,
-    updateEmployee,
-    delteEmployee,
-    getEmployee
-}
+  getAllEmployees,
+  createNewEmployee,
+  updateEmployee,
+  delteEmployee,
+  getEmployee,
+};
